6440ba806df41b4bf9693f034fb3f6d032848ecf
prudhvi <125962989+apocalypse9949@users.noreply.github.com>
2025-08-07 16:10:20 +0530

    Update AppController.java

--- crud_backend/crud_backend-main/src/main/java/com/klu/AppController.java
+++ crud_backend/crud_backend-main/src/main/java/com/klu/AppController.java
@@ -4,40 +4,38 @@ import java.util.List;
 
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.web.bind.annotation.CrossOrigin;
+import org.springframework.web.bind.annotation.DeleteMapping;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.PutMapping;
 import org.springframework.web.bind.annotation.RequestBody;
-import org.springframework.web.bind.annotation.DeleteMapping;
-//import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RestController;
 
 @RestController
 @CrossOrigin
 public class AppController {
-	@Autowired
-	Service s;
 
-	@PostMapping("/insert")
-	public String insert(@RequestBody Product p) {
-		return s.insertData(p);
-	}
+    @Autowired
+    private Service service;
 
-	@GetMapping("/display")
-	public List<Product> display() {
-		return s.displayData();
-	}
+    @PostMapping("/products")
+    public String insertProduct(@RequestBody Product product) {
+        return service.insertData(product);
+    }
 
-	@PutMapping("/update")
-	public String update(@RequestBody Product p) {
-		return s.updateData(p);
-	}
+    @GetMapping("/products")
+    public List<Product> getAllProducts() {
+        return service.displayData();
+    }
 
-	/* 
-	@DeleteMapping("/delete/{id}")
-	public String delete(@PathVariable int id) {
-		return s.deleteData(id);
-	}
-	*/
-	
+    @PutMapping("/products")
+    public String updateProduct(@RequestBody Product product) {
+        return service.updateData(product);
+    }
+
+    @DeleteMapping("/products/{id}")
+    public String deleteProduct(@PathVariable int id) {
+        return service.deleteData(id);
+    }
 }
